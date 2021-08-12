6b8d29e2c2c0e69749e9820b0edd8359bb338464
maurineimirandazup <maurinei.miranda@zup.com.br>
2021-08-12 10:58:02 -0300

    python template windows
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>

--- templates/create_formula/languages/python3-bin/build.bat
+++ templates/create_formula/languages/python3-bin/build.bat
@@ -2,32 +2,32 @@
 echo off
 SETLOCAL
 SET BIN_FOLDER=bin
+SET SRC_FOLDER=src
 SET BAT_FILE=%BIN_FOLDER%\run.bat
-SET SH_FILE=%BIN_FOLDER%\run.sh
+
 :build
     call :checkCommand python3
     call :checkCommand pip3
+    call :checkCommand pyinstaller
 
     mkdir %BIN_FOLDER%
-    xcopy /E /I src %BIN_FOLDER%
+    pip3 install -r %SRC_FOLDER%/requirements.txt
+
     CALL :BAT_WINDOWS
-    CALL :SH_LINUX
     CALL :CP_DOCKER
-    pip3 install -r %BIN_FOLDER%/requirements.txt
+    CALL :PYTHON_BIN
+    GOTO DONE
+
+:PYTHON_BIN
+    pyinstaller -y --distpath %BIN_FOLDER% --onefile --clean %SRC_FOLDER%\main.py
+    rmdir /S /Q %SRC_FOLDER%\__pycache__
+    rmdir /S /Q build
     GOTO DONE
 
 :BAT_WINDOWS
     echo @ECHO OFF > %BAT_FILE%
     echo SET mypath=%%~dp0 >> %BAT_FILE%
-    echo start /B /D "%%mypath%%" /WAIT python main.py >> %BAT_FILE%
-
-:SH_LINUX
-	echo #!/bin/bash > %SH_FILE%
-    echo if [ -f /.dockerenv ] ; then >> %SH_FILE%
-    echo pip3 install -r "$(dirname "$0")"/requirements.txt ^>^> /dev/null >> %SH_FILE%
-    echo fi >> %SH_FILE%
-    echo python3 "$(dirname "$0")"/main.py >> %SH_FILE%
-    GOTO DONE
+    echo start /B /D "%%mypath%%" /WAIT main.exe >> %BAT_FILE%
 
 :CP_DOCKER
     copy Dockerfile %BIN_FOLDER%
